bbd4ed65066a2ee1977d8d443dc63031138108f4
Evan Purkhiser <evanpurkhiser@gmail.com>
2021-03-17 21:40:00 -0700

    Updates

--- index.tsx
+++ index.tsx
@@ -1,75 +1,76 @@
-import "arrive";
+import 'arrive';
 
 const PRICE_REGEX = /\+\$?([0-9.]+)/;
 
-function handleFormVisible(formEl: HTMLFormElement) {
-  console.log(formEl);
+function handleFormVisible(formEl: Element) {
+  const labels = Array.from(formEl.querySelectorAll('label'));
 
-  const labels = [...formEl.querySelectorAll("label")];
+  const breakdownContainer = document.createElement('div');
+  breakdownContainer.classList.add('cost-breakdown');
 
-  const breakdownContainer = document.createElement("div");
-  breakdownContainer.classList.add("cost-breakdown");
-
-  const cost = document.createElement("h4");
-  cost.classList.add("cost");
+  const cost = document.createElement('h4');
+  cost.classList.add('cost');
 
   breakdownContainer.append(cost);
 
-  const buttons = formEl.querySelector(".form-button-wrapper");
-  buttons.insertAdjacentElement("beforebegin", breakdownContainer);
+  const buttons = formEl.querySelector('.form-button-wrapper');
+
+  if (buttons !== null) {
+    buttons.insertAdjacentElement('beforebegin', breakdownContainer);
+  }
 
   // Get all labels + prices associated to the label being selected
   const extraCostLabels = labels
-    .map((label) => {
+    .map(label => {
       const costMatch = label.innerText.match(PRICE_REGEX);
 
       if (costMatch === null) {
         return null;
       }
 
-      return { label: label, cost: costMatch[1] };
+      return {label: label, cost: costMatch[1]};
     })
-    .filter((value) => value !== null);
+    .filter(value => value !== null);
 
   const computeTotalPrice = () => {
     const price = extraCostLabels
-      .map((obj) =>
-        obj.label.querySelector("input").checked ? parseInt(obj.cost, 10) : 0
+      .map(obj =>
+        obj?.label?.querySelector('input')?.checked ? parseInt(obj.cost, 10) : 0
       )
       .reduce((acc, value) => acc + value, 0);
 
-    cost.innerText = "Estimated Additional Cost: $" + price.toFixed(2);
+    cost.innerText = 'Estimated Additional Cost: $' + price.toFixed(2);
   };
 
   // Assign handlers to compute the updated price each time the labels change
-  labels.forEach((label) => {
-    const input = label.querySelector("input");
+  labels.forEach(label => {
+    const input = label.querySelector('input');
 
     if (input) {
-      input.addEventListener("change", computeTotalPrice);
+      input.addEventListener('change', computeTotalPrice);
     }
   });
 
   computeTotalPrice();
 
   // Convert labels into selectable items
-  const fieldsets = [...formEl.querySelectorAll("fieldset")];
+  const fieldsets = Array.from(formEl.querySelectorAll('fieldset'));
 
-  fieldsets.forEach((fieldset) => {
-    const description = fieldset.querySelector<HTMLDivElement>(".description");
+  fieldsets.forEach(fieldset => {
+    const description = fieldset.querySelector<HTMLDivElement>('.description');
 
     if (description === null) {
       return;
     }
 
-    description.style.display = "none";
+    description.style.display = 'none';
 
-    const options = [...fieldset.querySelectorAll<HTMLDivElement>(".option")];
+    const options = Array.from(fieldset.querySelectorAll<HTMLDivElement>('.option'));
 
-    options.forEach((option) => {
-      option.querySelector("input").style.display = "none";
+    options.forEach(option => {
+      option.querySelector('input')!.style.display = 'none';
       const optionName = option.innerText.trim();
-      const optionLabel = option.querySelector("label");
+      const optionLabel = option.querySelector('label')!;
       const optionDesc = fieldset.querySelector(
         '.description [data-option-for="' + optionName + '"]'
       );
@@ -83,9 +84,9 @@ function handleFormVisible(formEl: HTMLFormElement) {
       optionLabel.append(optionDesc);
     });
 
-    const legends = [
-      ...fieldset.querySelectorAll<HTMLLegendElement>("fieldset legend"),
-    ];
+    const legends = Array.from(
+      fieldset.querySelectorAll<HTMLLegendElement>('fieldset legend')
+    );
 
     legends.forEach(function (legend) {
       const name = legend.innerText.trim();
